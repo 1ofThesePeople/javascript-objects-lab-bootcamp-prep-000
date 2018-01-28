892ddc6026a2e99649669833367ace9e6dfee9f1
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 09:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(map, dish, recipe) {
   var addition = {[dish]: recipe}
   return Object.assign({}, map, addition)
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(map, dish, recipe) {
+  map[dish] = recipe
+  return map
+}
